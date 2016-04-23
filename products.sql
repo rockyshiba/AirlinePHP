7b430b7bc9efda77c178d94ec46047da3a9aa7f5
rockyshiba <n01145868@humbermail.ca>
2016-04-22 23:40:50 -0400

    updated products table
    This table has values to play with

--- products.sql
+++ products.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2016 at 04:03 AM
+-- Generation Time: Apr 23, 2016 at 05:39 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -32,10 +32,19 @@ CREATE TABLE IF NOT EXISTS `products` (
   `price` decimal(18,2) DEFAULT NULL,
   `stock` int(255) DEFAULT NULL,
   `serial_num` int(11) DEFAULT NULL,
-  `description` int(11) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
   `category` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `price`, `stock`, `serial_num`, `description`, `category`) VALUES
+(1, 'Pillows', '10.99', 99, 111222333, 'A comfy pillow for your travels', 'Comfort'),
+(2, 'Slippers', '6.99', 99, 222333444, 'Be nice to your feet with these slippers.', 'Footwear'),
+(3, 'Leather shoes', '200.00', 30, 54133315, 'In a hurry? Lost your shoes? We got you covered!', 'Footwear');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
